b9656b2cbdb6cea21c8239db6e7899c673e83e0d
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-07-07 01:26:05 +0200

    Add DLL entry-point

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use std::ffi::c_void;
+use windows::Win32::Foundation::{BOOL, HINSTANCE};
+
+#[no_mangle]
+pub extern "stdcall" fn DllMain(_dll: HINSTANCE, _reason: u32, _reserved: *const c_void) -> BOOL {
+    true.into()
 }
