a980b46743d40b67d6d3c7e410a8b86dcf39423a
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-07-07 14:56:44 +0200

    Add d3d11 interop

--- src/utils/d3d11.rs
+++ src/utils/d3d11.rs
@@ -3,6 +3,9 @@ use std::{mem, ptr};
 use windows::core::HRESULT;
 use windows::Win32::Graphics::Direct3D::D3D_DRIVER_TYPE_HARDWARE;
 use windows::Win32::Graphics::Direct3D11;
+use windows::Win32::Graphics::Direct3D11::{
+    ID3D11Device, ID3D11DeviceContext, ID3D11RenderTargetView, ID3D11Texture2D,
+};
 use windows::Win32::Graphics::Direct3D11::{D3D11_CREATE_DEVICE_FLAG, D3D11_SDK_VERSION};
 use windows::Win32::Graphics::Dxgi::Common::{
     DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_MODE_DESC, DXGI_SAMPLE_DESC,
@@ -11,6 +14,21 @@ use windows::Win32::Graphics::Dxgi::{
     IDXGISwapChain, DXGI_SWAP_CHAIN_DESC, DXGI_SWAP_EFFECT_DISCARD, DXGI_USAGE_RENDER_TARGET_OUTPUT,
 };
 use windows::Win32::UI::WindowsAndMessaging;
+
+extern "C" {
+    pub fn device(swap_chain: *const IDXGISwapChain) -> *const ID3D11Device;
+    pub fn immediate_context(device: *const ID3D11Device) -> *const ID3D11DeviceContext;
+    pub fn desc(swap_chain: *const IDXGISwapChain) -> DXGI_SWAP_CHAIN_DESC;
+    pub fn buf(swap_chain: *const IDXGISwapChain) -> *const ID3D11Texture2D;
+    pub fn create_render_target(
+        device: *const ID3D11Device,
+        buf: *const ID3D11Texture2D,
+    ) -> *const ID3D11RenderTargetView;
+    pub fn render_target(
+        ctx: *const ID3D11DeviceContext,
+        target_view: *const ID3D11RenderTargetView,
+    );
+}
 
 pub type Present = extern "stdcall" fn(*const IDXGISwapChain, u32, u32) -> HRESULT;
 
