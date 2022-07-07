d155e54c0d08bab0c2a83af8f1f4233f19921e41
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-07-07 11:40:11 +0200

    Add present detour

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,43 @@
 mod utils;
 
+use anyhow::Ok;
+use detour::static_detour;
 use std::ffi::c_void;
+use std::thread;
+use utils::d3d11;
 use windows::core::HRESULT;
 use windows::Win32::Foundation::{BOOL, HINSTANCE};
 use windows::Win32::Graphics::Dxgi::IDXGISwapChain;
+use windows::Win32::System::LibraryLoader;
+use windows::Win32::System::SystemServices::DLL_PROCESS_ATTACH;
+
+static_detour! {
+    static PRESENT_DETOUR: extern "stdcall" fn(*const IDXGISwapChain, u32, u32) -> HRESULT;
+}
 
 #[no_mangle]
-pub extern "stdcall" fn DllMain(_dll: HINSTANCE, _reason: u32, _reserved: *const c_void) -> BOOL {
+pub extern "stdcall" fn DllMain(dll: HINSTANCE, reason: u32, _reserved: *const c_void) -> BOOL {
+    if reason == DLL_PROCESS_ATTACH {
+        unsafe {
+            LibraryLoader::DisableThreadLibraryCalls(dll);
+        }
+
+        thread::spawn(move || {
+            let present_origin = d3d11::present()?;
+
+            unsafe {
+                PRESENT_DETOUR
+                    .initialize(present_origin, present)?
+                    .enable()?;
+            }
+
+            Ok(())
+        });
+    }
+
     true.into()
 }
 
-fn present(_swap_chain: *const IDXGISwapChain, _sync_internal: u32, _flags: u32) -> HRESULT {
-    todo!()
+fn present(swap_chain: *const IDXGISwapChain, sync_internal: u32, flags: u32) -> HRESULT {
+    PRESENT_DETOUR.call(swap_chain, sync_internal, flags)
 }
